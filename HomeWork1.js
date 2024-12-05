958a9249bfe78a28cbc5c5c1331007585dbc910b
Witthawat <fernovenlove@gmail.com>
2024-12-05 23:39:17 +0700

    My First Commit

---
+++ HomeWork1.js
@@ -0,0 +1,10 @@
+function equation(input) {
+    // Use the eval function to evaluate the string
+    return eval(input);
+  }
+  
+  // Test cases
+  console.log(equation("1+1+1"));       // Output: 2
+  console.log(equation("7*4-2"));    // Output: 26
+  console.log(equation("1+1+1+1+1")); // Output: 5
+  
